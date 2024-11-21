ff212eba7dfab8b03ccd3aa1f02dc8f5983c737d
Michael Fatemi <myfatemi04@gmail.com>
2024-11-21 08:07:12 -0500

    training MLP on all crops + single crops

--- project/training/models.py
+++ project/training/models.py
@@ -15,3 +15,25 @@ class SimpleFeatureProjector(nn.Module):
         y = self.projection2(X_pooled)
 
         return y
+
+
+class AttentionPooler(nn.Module):
+    def __init__(self, n_input_features: int, n_pooling_features: int, n_classes: int):
+        super().__init__()
+
+        self.qk_dim = 256
+        self.v_dim = 256
+
+        self.projection1 = nn.Linear(n_input_features, n_pooling_features)
+        self.kv = nn.Linear(n_pooling_features, self.qk_dim + self.v_dim)
+        self.query = nn.Embedding(1, self.qk_dim)
+        self.projection2 = nn.Linear(n_pooling_features, n_classes)
+
+    def forward(self, X: torch.Tensor):
+        X = self.projection1(X)
+        k, v = torch.split(self.kv(X), [self.qk_dim, self.v_dim], dim=-1)
+        scores = torch.softmax((k @ self.query(torch.tensor(0))), dim=0)
+        X_pooled = v.T @ scores
+        y = self.projection2(X_pooled)
+
+        return y
--- project/training/trainer.py
+++ project/training/trainer.py
@@ -1,18 +1,21 @@
+import datetime
 import json
-import re
 from pathlib import Path
-from typing import Dict, List, Literal, cast
+from typing import List, cast
 
+import numpy as np
 import pandas as pd
 import torch
+import torch.nn.functional as F
+from matplotlib import pyplot as plt
+from sklearn.metrics import f1_score
 
-from project.training.models import SimpleFeatureProjector
+from project.data.dirs import DATA_PATH
+from project.data.load_patches import load_patches
+from project.training.models import AttentionPooler, SimpleFeatureProjector
 
-DATA_PATH = Path(__file__).parent.parent.parent / "data"
-PATCHES_DIR = DATA_PATH / "patch_features/virginia"
 
-
-# Instead of training from scratch, we may want to try using an autoencoder.
+# TO CONSIDER: Instead of training from scratch, we may want to try using an autoencoder.
 def unsupervised_pretraining():
     pass
 
@@ -20,32 +23,199 @@ def unsupervised_pretraining():
 def train():
     patches_per_county = load_patches()
 
-    # with open(DATA_PATH / "crop_ontology.json", "w") as f:
-    #     json.dump(generate_crop_ontology(), f)
+    results_dir = (
+        Path(__file__).parent.parent.parent
+        / "out"
+        / datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    )
 
     with open(DATA_PATH / "crop_ontology.json", "r") as f:
         crop_ontology = json.load(f)
+
+    df = pd.read_csv(DATA_PATH / "va_crops_2022.csv", index_col="county")
 
     # Train a CNN. For this, we will just take the mean-squared-error of each crop.
     # We will output two primary features:
     # (a) whether a crop is farmed in a county, and
     # (b) how many <units> of that crop is farmed there.
-    # We have 60 classes.
-    model = SimpleFeatureProjector(512, 256, 60)
+    # prediction_crops = ["barley_for_grain"]
+    # prediction_crops = [c["slug"] for c in crop_ontology]
 
-    for epoch in range(10):
-        for county, patches in patches_per_county.items():
+    supported_crops = []
+    for crop_slug in df.columns:
+        if not df[crop_slug].isna().all():
+            supported_crops.append(crop_slug)
+
+    for i, crop in enumerate(supported_crops):
+        print("Training model for crop", crop, f"({i+1}/{len(supported_crops)})")
+        _train(df, results_dir / "single_models" / crop, [crop], patches_per_county)
+
+    for crop in supported_crops:
+        print("Training combined model...")
+        _train(df, results_dir / "combined_model", supported_crops, patches_per_county)
+
+
+def _train(
+    df: pd.DataFrame,
+    results_dir: Path,
+    prediction_crops: List[str],
+    patches_per_county: dict,
+):
+    per_crop_results_dir = results_dir / "per_crop"
+    per_crop_results_dir.mkdir(parents=True, exist_ok=True)
+
+    model = SimpleFeatureProjector(512, 512, len(prediction_crops) * 2)
+    # model = AttentionPooler(512, 256, 60 * 2)
+    optim = torch.optim.Adam(model.parameters())
+
+    metrics = []
+    train_metrics = []
+
+    # calculate class weights.
+    class_weights = []
+    for crop_slug in prediction_crops:
+        class_weights.append(1 / (1 + df[crop_slug].isna().mean()))
+    class_weights = torch.tensor(class_weights, dtype=torch.float32)
+
+    crop_indexes_in_df = [list(df.columns).index(c) for c in prediction_crops]
+
+    epochs = 100
+    for epoch in range(epochs):
+        regression_trues = []
+        regression_predictions = []
+        presence_trues = []
+        presence_prediction_logits = []
+
+        for i, (county, patches) in enumerate(patches_per_county.items()):
             # Get crop counts for this county.
+
+            if county not in df.index:
+                continue
 
             patch_features = torch.stack(
                 [patch["resnet_features"] for patch in patches]
             )
-            print(patch_features.shape)
-            result = model(patch_features)
-            print(result)
-            break
+            outputs = model(patch_features)
+            productivities = torch.tensor(
+                df.loc[county].to_numpy()[crop_indexes_in_df],
+                device=outputs.device,
+                dtype=torch.float32,
+            )
+            has_crop = ~torch.isnan(productivities)
 
-        break
+            presence_prediction_logits_, regression_predictions_ = torch.split(
+                outputs, len(prediction_crops), dim=-1
+            )
+
+            presence_loss = (
+                torch.binary_cross_entropy_with_logits(
+                    presence_prediction_logits_, has_crop.float()
+                )
+                @ class_weights
+            ).mean()
+            regression_loss = (
+                ((regression_predictions_[has_crop] - productivities[has_crop]) ** 2)
+                @ class_weights[has_crop]
+            ).mean()
+
+            presence_prediction_logits.append(presence_prediction_logits_)
+            presence_trues.append(has_crop)
+            regression_predictions.append(regression_predictions_)
+            regression_trues.append(productivities)
+
+            loss = presence_loss + regression_loss
+            optim.zero_grad()
+            loss.backward()
+            optim.step()
+
+            metrics.append(
+                {
+                    "epoch": epoch,
+                    "epoch_step": i,
+                    "presence_loss": presence_loss.item(),
+                    "regression_loss": regression_loss.item(),
+                    "total_loss": loss.item(),
+                }
+            )
+
+        ### Calculate statistics for model performance. ###
+        # 1) f1-score, 2) RMSE.
+        presence_prediction_logits = (
+            torch.stack(presence_prediction_logits).T.detach().cpu().numpy()
+        )
+        # sigmoid(0) -> 0.5, and monotonically increasing.
+        presence_predictions = presence_prediction_logits >= 0
+        presence_trues = torch.stack(presence_trues).T.detach().cpu().numpy()
+        regression_predictions = (
+            torch.stack(regression_predictions).T.detach().cpu().numpy()
+        )
+        regression_trues = torch.stack(regression_trues).T.detach().cpu().numpy()
+
+        for index, crop_slug in enumerate(prediction_crops):
+            if not presence_trues[index].any():
+                presence_f1 = -1
+                rmse_if_present = -1
+            else:
+                # Calculates the RMSE for counties that have it.
+                presence_f1 = f1_score(
+                    presence_trues[index], presence_predictions[index]
+                )
+                rmse_if_present = np.sqrt(
+                    np.mean(
+                        (
+                            regression_trues[index][presence_trues[index]]
+                            - regression_predictions[index][presence_trues[index]]
+                        )
+                        ** 2
+                    )
+                )
+
+                # Update a *cumulative* epoch metrics file.
+                train_metrics.append(
+                    {
+                        "epoch": epoch,
+                        "crop_slug": crop_slug,
+                        "presence_f1": presence_f1,
+                        "rmse_if_present": rmse_if_present,
+                        "support": sum(presence_trues[index]),
+                    }
+                )
+
+        pd.DataFrame(train_metrics).to_csv(results_dir / f"train_metrics.csv")
+
+    # Create a plot of the results per crop.
+    train_metrics_df = pd.DataFrame(train_metrics)
+    for crop_slug, crop_train_metrics_df in train_metrics_df.groupby("crop_slug"):
+        crop_slug = cast(str, crop_slug)
+
+        plt.rcParams["figure.figsize"] = [8, 4]
+        plt.clf()
+        plt.subplot(1, 2, 1)
+        plt.title("Presence Classification F1 Score for " + crop_slug)
+        plt.xlabel("Training epoch")
+        plt.ylabel("f1 score")
+        plt.plot(crop_train_metrics_df["epoch"], crop_train_metrics_df["presence_f1"])
+
+        plt.subplot(1, 2, 2)
+        plt.title("Regression RMSE for " + crop_slug)
+        plt.xlabel("Training epoch")
+        plt.ylabel("RMSE")
+        plt.plot(
+            crop_train_metrics_df["epoch"], crop_train_metrics_df["rmse_if_present"]
+        )
+        plt.tight_layout()
+
+        ### TODO: Use an evaluation set instead of a training set.
+        (per_crop_results_dir / crop_slug).mkdir()
+        plt.savefig(per_crop_results_dir / crop_slug / "train_metrics_timeseries.png")
+        crop_train_metrics_df.to_csv(
+            per_crop_results_dir / crop_slug / "train_metrics.csv"
+        )
+
+    # Save final results
+    train_metrics_df[train_metrics_df["epoch"] == epochs - 1].to_csv(
+        results_dir / "final_train_metrics.csv"
+    )
 
 
 if __name__ == "__main__":
